1765c1d407da8275ed4f9c07ba1219404e816472
Taki <hashimoto19980924@gmail.com>
2023-01-23 20:30:45 +0900

    fix

---
+++ scripts/fetchOwnedNftMunbai2.js
@@ -0,0 +1,19 @@
+async function main() {
+    var web3 = new Web3(window.ethereum);
+    var viewer = new VWBL.VWBLViewer({
+        web3,
+        vwblNetworkUrl: 'https://dev.vwbl.network',
+        dataCollectorAddress: '0x70Fb88205D1163D276458E5D12D1fcC995BcB252'
+    });
+    var userAddress = (await web3.eth.getAccounts())[0];
+    var ownedItems = await viewer.listOwnedNFTMetadata(userAddress);
+    var sortedOwnedItems = ownedItems.filter((v) => v).reverse();
+    console.log('owned nfts:', sortedOwnedItems);
+    sortedOwnedItems.forEach(v => {
+        var titleList = document.createElement('li');
+        titleList.textContent = v.name;
+        document.getElementById('titleList').appendChild(titleList);
+    });
+}
+
+main();

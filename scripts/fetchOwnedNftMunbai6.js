2feabbecb285ecf5b963cc26ce6399e4947c1fe0
Tatsuki <40402518+thashimoto1998@users.noreply.github.com>
2023-01-23 21:26:01 +0900

    Create fetchOwnedNftMunbai6.js

---
+++ scripts/fetchOwnedNftMunbai6.js
@@ -0,0 +1,23 @@
+async function main() {
+    const web3 = new Web3(window.ethereum);
+    const viewer = new VWBL.VWBLViewer({
+        web3,
+        vwblNetworkUrl: 'https://dev.vwbl.network',
+        dataCollectorAddress: '0x70Fb88205D1163D276458E5D12D1fcC995BcB252'
+    });
+    const userAddress = (await web3.eth.getAccounts())[0];
+    const ownedItems = await viewer.listOwnedNFTMetadata(userAddress);
+    const sortedOwnedItems = ownedItems.filter((v) => v).reverse();
+    console.log('owned nfts:', sortedOwnedItems);
+    for (const v of sortedOwnedItems) {
+        var image = document.createElement('img');
+        var res = await fetch(v.image);
+        console.log('res', res);
+        const blob = await res.blob();
+        image.src = URL.createObjectURL(blob);
+        image.alt = v.name;
+        document.getElementById('imageList').appendChild(image);
+    };
+}
+
+main();
